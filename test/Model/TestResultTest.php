0004a083707d241dbdde5e7bb72f8ea8f2deaf0f
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2026-04-28 14:46:29 +0700

    Add titlePath API (#72)

---
+++ test/Model/TestResultTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Qameta\Allure\Test\Model;
+
+use PHPUnit\Framework\TestCase;
+use Qameta\Allure\Model\TestResult;
+
+use function json_decode;
+use function json_encode;
+
+/**
+ * @covers \Qameta\Allure\Model\TestResult
+
+ */
+class TestResultTest extends TestCase
+{
+    public function testGetTitlePathReturnsEmptyArrayByDefault(): void
+    {
+        $testResult = new TestResult("e42e48d0-7a3f-4fba-8518-8e6ada04af1d");
+
+        $titlePath = $testResult->getTitlePath();
+
+        self::assertEquals($titlePath, []);
+    }
+
+    public function testSetTitlePathReturnsObjectItself(): void
+    {
+        $testResult = new TestResult("e42e48d0-7a3f-4fba-8518-8e6ada04af1d");
+
+        $actual = $testResult->setTitlePath();
+
+        self::assertSame($actual, $testResult);
+    }
+
+    public function testTitlePathCanBeSet(): void
+    {
+        $testResult = new TestResult("e42e48d0-7a3f-4fba-8518-8e6ada04af1d");
+        $testResult->setTitlePath("foo", "bar", "baz");
+
+        $titlePath = $testResult->getTitlePath();
+
+        self::assertEquals($titlePath, ["foo", "bar", "baz"]);
+    }
+
+    public function testTitlePathJsonSerialization(): void
+    {
+        $testResult = new TestResult("e42e48d0-7a3f-4fba-8518-8e6ada04af1d");
+        $testResult->setTitlePath("foo", "bar", "baz");
+
+        /** @psalm-var object{titlePath: list<string>} $decodedJson */
+        $decodedJson = json_decode(json_encode($testResult));
+
+        self::assertEquals($decodedJson->titlePath, ["foo", "bar", "baz"]);
+    }
+}
